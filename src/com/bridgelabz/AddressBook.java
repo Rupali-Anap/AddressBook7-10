84f1ce2449b183a1981d19047b09891add8df2e0
Rupali Anap <anaprupali333@gmail.com>
2023-01-31 08:18:28 -0800

    Added: Added Contacts

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,21 +1,76 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
+
+        private ArrayList<Contact> ContactList;
+
+        public AddressBook() {
+            ContactList = new ArrayList<Contact>();
+        }
+
+        public void addContact(Contact createPerson) {
+            System.out.println("Adding a New Contact for " + createPerson.firstName);
+            ContactList.add(createPerson);
+        }
+
+        public Contact getContactInput() {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter First Name: ");
+            String firstName = sc.nextLine();
+            System.out.println("Enter Last Name: ");
+            String lastName = sc.nextLine();
+            System.out.println("Enter the Address: ");
+            String address = sc.nextLine();
+            System.out.println("Enter the City: ");
+            String city = sc.nextLine();
+            System.out.println("Enter the State: ");
+            String state = sc.nextLine();
+            System.out.println("Enter the Zip: ");
+            int zip = sc.nextInt();
+            System.out.println("Enter the Phone No: ");
+            String phoneNumber = sc.nextLine();
+            sc.nextLine();
+            System.out.println("Enter the Email: ");
+            String email = sc.nextLine();
+
+            Contact contact = new Contact(firstName, lastName, address, city, state, email, zip, phoneNumber);
+            return contact;
+        }
 
         public static void main(String[] args) {
 
-            System.out.println("Welcome to Address Book System\n");
-            Contact info = new Contact("Rupali", "Anap", "Yeola", "Nashik", "Maharashtra", "anaprupali333@gmail.com", 422222, "7083409631");
-            System.out.println("Firstname: " + info.getFirstName());
-            System.out.println("Lastname: " + info.getLastName());
-            System.out.println("Address: " + info.getAddress());
-            System.out.println("City: " + info.getCity());
-            System.out.println("State: " + info.getState());
-            System.out.println("Email: " + info.getEmail());
-            System.out.println("Zip: " + info.getZip());
-            System.out.println("PhoneNumber: " + info.getPhoneNumber());
+            System.out.println("Welcome to the Address Book System");
+            int ch = 0;
+            Scanner sc = new Scanner(System.in);
+            AddressBook AD = new AddressBook();
 
+            while (true) {
+                System.out.println("1.Adding Contact \n2.Exit");
+                System.out.println("Enter a Your Choice: ");
+                ch = sc.nextInt();
 
+                switch (ch) {
+                    case 1:
+                        Contact contact = AD.getContactInput();
+                        AD.addContact(contact);
 
+                        System.out.println("Contact Added Successfully ");
+                        break;
+
+                    case 2:
+                        System.out.println("Thank You We are Exiting");
+                        return;
+
+                    default:
+                        System.out.println("Sorry You are Type Wrong Choice");
+                }
+            }
         }
-}
+    }
+
+
+
